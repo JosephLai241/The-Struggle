f11ade42dc0beb529a5fe772ca033c33411f341e
Joseph Lai <joseph@arch.localdomain>
2020-05-17 20:37:19 -0400

    Changed flag for CLI

--- src/cli.py
+++ src/cli.py
@@ -5,14 +5,14 @@ import argparse
 import sys
 
 ### Argparse text
-usage = "track.py [-n COMPANY_NAME] [-u COMPANY_NAME] [-d COMPANY_NAME] [-l]"
+usage = "track.py [-a COMPANY_NAME] [-u COMPANY_NAME] [-d COMPANY_NAME] [-l]"
 description = "Struggle Tracker - A program that helps you track your job applications"
 epilog = r"""
 EXAMPLES
 
     Add job application at Stack Overflow to the spreadsheet:
 
-        $ ./track.py -n "Stack Overflow"
+        $ ./track.py -a "Stack Overflow"
 
     Update job application at Apple in the spreadsheet. Returns list of matches at Apple if you applied to more than one job at the company:
 
@@ -35,7 +35,7 @@ def parse_args():
                                     epilog = epilog)
 
     scraper = parser.add_argument_group("Tracking Options")
-    scraper.add_argument("-n","--new",nargs=1,metavar="",help="enter new company to track")
+    scraper.add_argument("-a","--add",nargs=1,metavar="",help="add new company to track")
     scraper.add_argument("-u","--update",nargs=1,metavar="",help="update an existing company in the spreadsheet")
     scraper.add_argument("-d","--delete",nargs=1,metavar="",help="delete an existing company in the spreadsheet")
     scraper.add_argument("-l","--list",action="store_true",help="list all saved job applications")
