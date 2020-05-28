e45a1bf357b9703c53668f9d34b910f432c2d3e2
Joseph Lai <joseph@arch.localdomain>
2020-05-28 17:11:29 -0400

    Adding whitespace for improved readability

--- track.py
+++ track.py
@@ -13,26 +13,27 @@ import src.programs.insight as insight
 import src.programs.list as listings
 import src.programs.new as new
 import src.programs.update as update
+
 from src.functions import (delete_functions, insight_functions, list_functions,
                            new_functions, search_functions, update_functions)
 
 def main():
-    parser,args = cli.parse_args()
+    parser, args = cli.parse_args()
     if args.add:
         ### Add new job to spreadsheet
-        new.add_job(args,new_functions,parser)
+        new.add_job(args, new_functions, parser)
     if args.update:
         ### Update an existing job in the spreadsheet
-        update.update_job(args,update_functions,parser,search_functions)
+        update.update_job(args, update_functions, parser, search_functions)
     if args.delete:
         ### Delete an existing job in the spreadsheet
-        delete.delete_job(args,delete_functions,parser,search_functions)
+        delete.delete_job(args, delete_functions, parser, search_functions)
     if args.list:
         ### List all existing jobs in the spreadsheet
-        listings.list_jobs(args,list_functions,parser)
+        listings.list_jobs(args, list_functions, parser)
     if args.insights:
         ### Return job application insights
-        insight.print_insights(args,insight_functions,parser)
+        insight.print_insights(args, insight_functions, parser)
 
 
 if __name__ == "__main__":
