7bfeb5de5876f91cde578fecfedc663801ccf41d
Joseph Lai <joseph@arch.localdomain>
2020-05-31 01:27:16 -0400

    Add should be good to go, but review print_job()

--- Rust/the-struggle/src/add.rs
+++ Rust/the-struggle/src/add.rs
@@ -1,30 +1,32 @@
+use crate::mcsv;
 use crate::model::Job;
 
 use chrono::prelude::*;
 use std::io;
+use std::process;
 
 fn get_title(company: &String) -> String {
     // Enter a job title.
-    let mut title = String::new();
 
+    let mut title = String::new();
     loop {
         println!("What is the title of the position you are applying for at {}?", company);
         match io::stdin().read_line(&mut title) {
             Ok(_) => {
                 let input = title.trim().to_string();
+
                 if input.is_empty() {
                     println!("Please enter a job title.\n");
-                } else {
-                return title.trim().to_string();
-                }
+                } else { return title.trim().to_string(); }
             },
-            Err(e) => {println!("Error! {:?}", e);}
+            Err(e) => { println!("Error! {:?}", e); }
         }
     }
 }
 
 fn get_status() -> String {
     // Enter job application status.
+
     let status_options: Vec<String> = vec!["PENDING".to_string(), "IN PROGRESS".to_string(), 
                                             "OFFER RECEIVED".to_string(), "HIRED".to_string(), 
                                             "REJECTED".to_string()];
@@ -42,12 +44,15 @@ fn get_status() -> String {
     let mut status = String::new();
     loop {
         println!("{}", status_prompt);
-
         match io::stdin().read_line(&mut status) {
             Ok(_) => {
                 if status.trim().is_empty() {
-                    println!("Please select a status option.")
-                } else{
+                    println!("Please select a valid status option.");
+                    status.clear();
+                } else if !status.trim().chars().all(char::is_numeric) {
+                    println!("Please select a valid status option.");
+                    status.clear();
+                } else {
                     let status_int = status.trim().parse::<usize>().unwrap();
                     
                     if std::usize::MIN <= status_int && 
@@ -66,21 +71,20 @@ fn get_status() -> String {
 
 fn get_notes() -> String {
     // Enter notes about the job listing.
-    let mut notes = String::new();
 
+    let mut notes = String::new();
     loop {
         println!("\nEnter any notes for this position:");
         match io::stdin().read_line(&mut notes) {
-            Ok(_) => {
-                return notes.trim().to_string();
-            },
-            Err(e) => {println!("Error! {:?}", e);}
+            Ok(_) => { return notes.trim().to_string(); },
+            Err(e) => { println!("Error! {:?}", e); }
         }
     }
 }
 
 pub fn add_job(company: String) -> Job {
     // Return Job struct from date, job title, job status, and notes.
+
     let local: DateTime<Local> = Local::now();
 
     let date = local.format("%m-%d-%Y %H:%M:%S").to_string();
@@ -91,8 +95,9 @@ pub fn add_job(company: String) -> Job {
     return Job::new_job(date, company, title, status, notes);
 }
 
-fn format_print(c_len: &usize, t_len: usize, n_len: &usize, vector: &Vec<String>) -> usize {
+fn format_print(c_len: &usize, t_len: usize, n_len: &usize, vector: &Vec<&String>) -> usize {
     // Format how the table header and jobs are printed in the terminal.
+
     let line = format!(
         "\n{:<21} {:<c_len$} {:<t_len$} {:<16} {:<n_len$}",
         vector[0], vector[1], vector[2], vector[3], 
@@ -105,27 +110,64 @@ fn format_print(c_len: &usize, t_len: usize, n_len: &usize, vector: &Vec<String>
     return max;
 }
 
-pub fn print_job(job: Job) -> Vec<String> {
+fn print_job(job: &Job) {
     // Print the new job listing to add to the spreadsheet.
+
     let job_categories: Vec<String> = vec!["DATE ADDED".to_string(), "COMPANY".to_string(), 
                                             "JOB TITLE".to_string(), "STATUS".to_string(), 
                                             "NOTES".to_string()];
+    
     let c_len = &job.company.len() + 4;
-    let t_len = if &job.title.len() > &10 {&job.title.len() + 2} 
-                else {job_categories[2].len()};
+    let t_len = if &job.title.len() > &10 { &job.title.len() + 2 } 
+                else { job_categories[2].len() };
     let n_len = &job.notes.len() + 2;
 
-    let max = format_print(&c_len, t_len, &n_len, &job_categories);
+    // TODO: Find a way around borrow issue. Traits?
+    let jc_borrowed: Vec<&String> = vec![&job_categories[0], &job_categories[1], 
+                                        &job_categories[2], &job_categories[3], 
+                                        &job_categories[4]];
+    let max = format_print(&c_len, t_len, &n_len, &jc_borrowed);
     println!("{:-<width$}", "", width = max - 2);
     
-    let details: Vec<String> = vec![job.date, job.company, job.title, job.status,
-    job.notes];
+    let details: Vec<&String> = vec![&job.date, &job.company, &job.title, &job.status,
+                                    &job.notes];
 
     format_print(&c_len, t_len, &n_len, &details);
-
-    return details;
 }
 
 pub fn confirm_new_job(new_job: Job) {
-    
+    // Print the job listing to add, then ask user to confirm append. On confirm,
+    // the program will append the job to the spreadsheet.
+
+    print_job(&new_job);
+
+    let options: Vec<String> = vec!["Y".to_string(), "N".to_string()];
+    let mut confirm_in = String::new();
+    loop {
+        println!("\nConfirm? [Y/N]");
+        match io::stdin().read_line(&mut confirm_in) {
+            Ok(_) => { 
+                let confirm = confirm_in.trim().to_uppercase();
+                if options.iter().any(|ch| ch == &confirm) {
+                    // If input in options
+                    if confirm == options[0] {  
+                        // If input is "Y"
+                        if let Err(err) = mcsv::write_new_job(&new_job) {
+                            println!("An error has occured! {}", err);
+                            process::exit(1);
+                        } else { return; } 
+                    } else {
+                        // If input is "N"
+                        println!("\nEXITING.");
+                        process::exit(1);
+                    }
+                } else { 
+                    // If invalid option is entered
+                    println!("\nNot an option!");
+                    confirm_in.clear(); 
+                }
+            },
+            Err(e) => { println!("Error! {:?}", e); }
+        }
+    }
 }
