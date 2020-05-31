37363c2aec195b46d47d55a6e9266a9508943086
Joseph Lai <joseph@arch.localdomain>
2020-05-31 01:28:13 -0400

    Cleaned up main.rs

--- Rust/the-struggle/src/main.rs
+++ Rust/the-struggle/src/main.rs
@@ -1,18 +1,14 @@
 mod cli;
-mod m_csv;
+mod mcsv;
 mod model;
 mod add;
-
-use std::string::String;
 
 fn main() {
     let flags = cli::get_args();
 
     if let Some(company) = flags.add {
         let new_job = add::add_job(company);
-        let details = add::print_job(new_job);
-        // println!("{:?}", details);
-
+        add::confirm_new_job(new_job);
     } else if let Some(company) = flags.update {
         println!("UPDATE {}", company);
 
@@ -26,15 +22,4 @@ fn main() {
         println!("DISPLAY INSIGHTS BY {}", display);
 
     }
-
-    // USE UNWRAP() TO GET MASTER BTREEMAP
-    // let master = m_csv::get_jobs().unwrap();
-    // println!("PRINTING FOR MASTER NOW\n");
-    // println!("PRINTING MASTER LENGTH\n");
-    // println!("{:?}", master.len() - 1);
-    // for i in 0u16..master.len() as u16 {
-    //     println!("{}", master.get(&i).unwrap().company);
-    //     println!("{}", master.get(&i).unwrap().title);
-    //     println!();
-    // }
 }
