762cbc9fdbae638c53ea29b8cb579135c96e7d31
Joseph Lai <jlai24142@gmail.com>
2020-06-30 05:17:38 -0400

    Editing comments for documentation

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,5 @@
-#[derive(Debug)]
+/// Job schema.
 pub struct Job {
-    /// Job schema.
     pub date: String,
     pub company: String,
     pub title: String,
@@ -8,8 +7,8 @@ pub struct Job {
     pub notes: String
 }
 
+/// Create a new Job object using the Job struct.
 impl Job {
-    /// Create a new Job object using the Job struct.
     pub fn new_job(date: String, company: String, title: String, status: String,
         notes: String) -> Job {
             Job {date: date, company: company, title: title, status: status,
