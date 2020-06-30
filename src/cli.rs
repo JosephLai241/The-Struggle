84e813cef48dd0d909e802f7e7591785fbb395b3
Joseph Lai <jlai24142@gmail.com>
2020-06-29 23:52:01 -0400

    Removing some functionality that is not possible with the current PrettyTable

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,8 @@
-use clap::arg_enum;
+// use clap::arg_enum;
 use structopt::StructOpt;
 
 /// This struct contains all flags that are used in this program.
-#[derive(Debug, StructOpt)]
+#[derive(StructOpt)]
 #[structopt(
     name = "The Struggle", 
     about = "A Rust command line tool for tracking your job applications"
@@ -37,48 +37,66 @@ pub struct Args {
         short = "l", 
         long = "list", 
         help = "List all saved job applications",
-        possible_values = &Sort::variants(),
-        case_insensitive = true
     )]
-    pub list: Option<String>,
+    pub list: bool,
 
     /// Flag for displaying job application insights.
     #[structopt(
         short = "i", 
         long = "insights", 
         help = "Display job application insights",
-        possible_values = &Display::variants(),
-        case_insensitive = true
     )]
-    pub insights: Option<String>,
+    pub insights: bool,
 
+////////////////////////////////////////////////////////////////////////////////
+
+    // /// Flag for listing all existing jobs. Rust's PrettyTable does not have a sort
+    // /// option yet, so this functionality is currently not available.
+    // #[structopt(
+    //     short = "l", 
+    //     long = "list", 
+    //     help = "List all saved job applications",
+    //     possible_values = &Sort::variants(),
+    //     case_insensitive = true
+    // )]
+    // pub list: Option<String>,
+
+    // /// Flag for displaying job application insights. Will consider adding this
+    // /// option back if it is requested.
+    // #[structopt(
+    //     short = "i", 
+    //     long = "insights", 
+    //     help = "Display job application insights",
+    //     possible_values = &Display::variants(),
+    //     case_insensitive = true
+    // )]
+    // pub insights: Option<String>,
 }
 
-arg_enum! {
-    /// Enum for list sorting options.
-    #[derive(Debug)]
-    enum Sort {
-        Date,
-        Reversed,
-        Company,
-        Title,
-        Status,
-        Notes
-    }
-}
+// arg_enum! {
+//     /// Enum for list sorting options. Rust's PrettyTable does not have a sort
+//     /// option yet, so this functionality is currently not available.
+//     enum Sort {
+//         Date,
+//         Reverse,
+//         Company,
+//         Title,
+//         Status,
+//         Notes
+//     }
+// }
 
-arg_enum! {
-    /// Enum for insight display options.
-    #[derive(Debug)]
-    enum Display {
-        All,
-        Pending,
-        InProgress,
-        Offers,
-        Hired,
-        Rejected
-    }
-}
+// arg_enum! {
+//     /// Enum for insight display options.
+//     enum Display {
+//         All,
+//         Pending,
+//         InProgress,
+//         Offers,
+//         Hired,
+//         Rejected
+//     }
+// }
 
 /// Return Args struct.
 pub fn get_args() -> Args {
