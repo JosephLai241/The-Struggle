3761fef7034f7e698ef31768cab669bac3dd1327
Joseph Lai <jlai24142@gmail.com>
2020-06-29 23:52:51 -0400

    Adding list functionality. Some formatting changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod cli;
+mod list;
 mod mcsv;
 mod model;
 mod add;
@@ -11,19 +12,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     if let Some(company) = flags.add {
         let new_job = add::add_job(company);
         add::confirm_new_job(new_job)?;
+
     } else if let Some(company) = flags.update {
         println!("UPDATE {}", company);
-        let master = mcsv::get_jobs()?;
-
-
+        
     } else if let Some(company) = flags.delete {
         println!("DELETE {}", company);
 
-    } else if let Some(sort) = flags.list {
-        println!("SORT LIST BY {}", sort);
+    } else if flags.list == true {
+        let master = mcsv::get_jobs()?;
+        list::list_jobs(master);
 
-    } else if let Some(display) = flags.insights {
-        println!("DISPLAY INSIGHTS BY {}", display);
+    } else if flags.insights == true {
+        println!("DISPLAY INSIGHTS");
 
     } else {
         println!("NO ARGUMENTS GIVEN.");
