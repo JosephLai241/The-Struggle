994d0c000b9e81eadb843c117acb1e9b57529dad
Joseph Lai <jlai24142@gmail.com>
2020-07-01 19:17:56 -0400

    Minor refactoring

--- src/delete.rs
+++ src/delete.rs
@@ -9,14 +9,14 @@ fn update_keys(master: &mut BTreeMap<u16, Job>) -> BTreeMap<u16, Job> {
 
     for (index, job) in master.values_mut().enumerate() {
         let index = index as u16;
-
-        let date = job.date.clone();
-        let company = job.company.clone();
-        let title = job.title.clone();
-        let status = job.status.clone();
-        let notes = job.notes.clone();
-
-        update.insert(index, Job::new_job(date, company, title, status, notes));
+        
+        update.insert(index, Job::new_job(
+            job.date.clone(),
+            job.company.clone(),
+            job.title.clone(),
+            job.status.clone(),
+            job.notes.clone()
+        ));
     }
 
     update
@@ -25,7 +25,6 @@ fn update_keys(master: &mut BTreeMap<u16, Job>) -> BTreeMap<u16, Job> {
 /// Delete the selected job from the master BTreeMap. Then rewrite the spreadsheet.
 pub fn delete_job(job_index: u16, master: &mut BTreeMap<u16, Job>) {
     master.remove(&job_index);
-    
     let mut update = update_keys(master);
     overwrite(&mut update).expect("Failed to overwrite spreadsheet.");
 }
