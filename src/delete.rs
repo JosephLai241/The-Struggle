d908a27da750492e0f933064ee61484815b47ea8
Joseph Lai <jlai24142@gmail.com>
2020-07-02 05:31:09 -0400

    Now prints the job listing and asks for deletion confirmation

--- src/delete.rs
+++ src/delete.rs
@@ -1,7 +1,12 @@
 use crate::mcsv::overwrite;
 use crate::model::Job;
 
+use ansi_term::*;
+use prettytable::*;
+
 use std::collections::BTreeMap;
+use std::io;
+use std::process;
 
 /// Update the keys within the BTreeMap after deleting a job.
 fn update_keys(master: &mut BTreeMap<u16, Job>) -> BTreeMap<u16, Job> {
@@ -24,7 +29,53 @@ fn update_keys(master: &mut BTreeMap<u16, Job>) -> BTreeMap<u16, Job> {
 
 /// Delete the selected job from the master BTreeMap. Then rewrite the spreadsheet.
 pub fn delete_job(job_index: u16, master: &mut BTreeMap<u16, Job>) {
-    master.remove(&job_index);
-    let mut update = update_keys(master);
-    overwrite(&mut update).expect("Failed to overwrite spreadsheet.");
+    println!("\n{}", Colour::Cyan.bold().paint("SELECTED JOB"));
+    let mut to_delete = Table::new();
+
+    to_delete.add_row(
+        row![
+            bFl => 
+            "DATE ADDED", 
+            "COMPANY", 
+            "JOB TITLE", 
+            "STATUS", 
+            "NOTES"
+        ]
+    );
+
+    to_delete.add_row(row![
+        master.get(&job_index).unwrap().date.to_string(),
+        master.get(&job_index).unwrap().company.to_string(),
+        master.get(&job_index).unwrap().title.to_string(),
+        master.get(&job_index).unwrap().status.to_string(),
+        master.get(&job_index).unwrap().notes.to_string(),
+    ]);
+
+    to_delete.printstd();
+
+    loop {
+        let mut confirm_delete = String::new();
+
+        println!("\n{}", Style::new().bold().paint("Confirm deletion? [Y/N]"));
+
+        match io::stdin().read_line(&mut confirm_delete) {
+            Ok(_) => { 
+                match confirm_delete.trim().to_uppercase().as_str() {
+                    "Y" => {
+                        master.remove(&job_index);
+                        let mut update = update_keys(master);
+                        overwrite(&mut update).expect("Failed to overwrite spreadsheet.");
+                        
+                        break;
+                    },
+                    "N" => {
+                        println!("\n{}\n", Colour::Red.bold().paint("CANCELLING."));
+                        process::exit(1);
+                    },
+                    _ => println!("\n{}", Colour::Red.bold().paint("Not an option!"))
+                }
+            },
+            Err(e) => { println!("Error! {:?}", e); }
+        }
+    }
 }
