ebe61fd3274d8647e28a08b1f778c5717b3b95e6
Joseph Lai <jlai24142@gmail.com>
2020-07-03 11:33:14 -0400

    Implemented struct attribute matching to run the program

--- src/main.rs
+++ src/main.rs
@@ -15,47 +15,39 @@ use ansi_term::*;
 fn main() {
     titles::main_title();
 
-    let flags = cli::get_args();
+    match cli::get_args() {
+        cli::Args { add: Some(company), .. } => {
+            let new_job = add::add_job(company);
+            add::confirm_add(new_job);
+        },
+        cli::Args { update: Some(company), .. } => {
+            let mut master = mcsv::get_jobs().unwrap();
 
-    // Add a new job to the spreadsheet.
-    if let Some(company) = flags.add {
-        let new_job = add::add_job(company);
-        add::confirm_add(new_job);
+            let match_indexes = search::print_matches(&company, &master);
+            let job_index = search::select_match(match_indexes);
 
-    // Update a job in the spreadsheet.
-    } else if let Some(company) = flags.update {        
-        let mut master = mcsv::get_jobs().unwrap();
+            let section_int = update::select_attribute();
+            let update = update::update_attribute(section_int);
 
-        let match_indexes = search::print_matches(&company, &master);
-        let job_index = search::select_match(match_indexes);
+            update::update_job(job_index, &mut master, update);
+        },
+        cli::Args { delete: Some(company), .. } => {
+            let mut master = mcsv::get_jobs().unwrap();
 
-        let section_int = update::select_attribute();
-        let update = update::update_attribute(section_int);
+            let match_indexes = search::print_matches(&company, &master);
+            let job_index = search::select_match(match_indexes);
 
-        update::update_job(job_index, &mut master, update);
-        
-    // Delete a job from the spreadsheet.
-    } else if let Some(company) = flags.delete {
-        let mut master = mcsv::get_jobs().unwrap();
-
-        let match_indexes = search::print_matches(&company, &master);
-        let job_index = search::select_match(match_indexes);
-
-        delete::delete_job(job_index, &mut master);
-
-    // List all stored jobs.
-    } else if flags.list == true {
-        let master = mcsv::get_jobs().unwrap();
-        list::list_jobs(master);
-
-    // Display insights for each application status.
-    } else if flags.insights == true {
-        let master = mcsv::get_jobs().unwrap();
-        let job_stats = insights::get_stats(master);
-        insights::display_insights(job_stats);
-
-    // If no arguments were entered.
-    } else {
-        println!("{}\n", Colour::Red.bold().paint("NO ARGUMENTS GIVEN 👎"));
+            delete::delete_job(job_index, &mut master);
+        },
+        cli::Args { list: true, .. } => {
+            let master = mcsv::get_jobs().unwrap();
+            list::list_jobs(master);
+        },
+        cli::Args { insights: true, .. } => {
+            let master = mcsv::get_jobs().unwrap();
+            let job_stats = insights::get_stats(master);
+            insights::display_insights(job_stats);
+        },
+        _ => println!("{}\n", Colour::Red.bold().paint("NO ARGUMENTS GIVEN 👎"))
     }
 }
