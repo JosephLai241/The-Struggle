f7b425d73e122c429864814e095919859493ee92
Joseph Lai <jlai24142@gmail.com>
2020-07-03 01:26:41 -0400

    Adding unit test

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,5 @@
 /// Job schema.
+#[derive(Debug, PartialEq)]
 pub struct Job {
     pub date: String,
     pub company: String,
@@ -13,5 +14,28 @@ impl Job {
         notes: String) -> Job {
             Job {date: date, company: company, title: title, status: status,
                 notes: notes}
+    }
+}
+
+#[cfg(test)]
+mod test_model {
+    use super::*;
+
+    #[test]
+    fn test_new_job() {
+        let test_job = Job {
+            date: "07-02-2020 21:09:39".to_string(),
+            company: "ECorp".to_string(),
+            title: "Security Engineer".to_string(),
+            status: "HIRED".to_string(),
+            notes: "My name is Elliot".to_string()
+        };
+
+        assert_eq!(Job::new_job(
+            "07-02-2020 21:09:39".to_string(),
+            "ECorp".to_string(),
+            "Security Engineer".to_string(),
+            "HIRED".to_string(),
+            "My name is Elliot".to_string()), test_job);
     }
 }
