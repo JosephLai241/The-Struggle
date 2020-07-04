c1b7eb2b22d9fef79652a421a7d355a07a2e5a91
Joseph Lai <jlai24142@gmail.com>
2020-07-04 00:34:49 -0400

    Formatting changes, updated doc comments

--- src/list.rs
+++ src/list.rs
@@ -7,7 +7,7 @@ use prettytable::*;
 
 use std::collections::BTreeMap;
 
-/// Add each stored job as a row in the PrettyTable.
+/// Add each stored job listing as a row in the PrettyTable.
 fn add_rows(job_table: &mut Table, master: BTreeMap<u16, Job>) {
     for i in 0u16..master.len() as u16 {
         let job_details = vec![
@@ -25,26 +25,25 @@ fn add_rows(job_table: &mut Table, master: BTreeMap<u16, Job>) {
     }
 }
 
-/// Print the stored jobs in a PrettyTable.
+/// Print the stored job listings in a PrettyTable.
 pub fn list_jobs(master: BTreeMap<u16, Job>) {
     let mut job_table = Table::new();
 
     job_table.add_row(
         row![
-            bFl => 
+            bil => 
             "DATE ADDED", 
             "COMPANY", 
             "JOB TITLE", 
             "STATUS", 
             "NOTES"
-        ]
-    );
+    ]);
 
     add_rows(&mut job_table, master);
 
     job_table.set_titles(Row::new(vec![
         Cell::new(&format!("{} TRACKED JOB APPLICATIONS", job_table.len() - 1))
-            .style_spec("bcH5")
+            .style_spec("bicH5")
     ]));
     
     job_table.set_format(format_table());
