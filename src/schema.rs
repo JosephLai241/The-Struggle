1f9eb5d60066056b0dd1f0e863e606755360bd7f
Joseph Lai <jlai24142@gmail.com>
2023-08-05 12:35:11 -0500

    Adding the schema.rs file generated by diesel

---
+++ src/schema.rs
@@ -0,0 +1,29 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    job_data (id) {
+        id -> Nullable<Integer>,
+        company -> Text,
+        date_added -> Text,
+        link -> Nullable<Text>,
+        notes -> Nullable<Text>,
+        status -> Text,
+        stint -> Nullable<Integer>,
+        title -> Text,
+    }
+}
+
+diesel::table! {
+    stints (id) {
+        id -> Nullable<Integer>,
+        date_added -> Text,
+        stint -> Text,
+    }
+}
+
+diesel::joinable!(job_data -> stints (stint));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    job_data,
+    stints,
+);
