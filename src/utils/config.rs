a9a988a774278f8a86604ea9e8573c1689c53ecb
Joseph Lai <jlai24142@gmail.com>
2023-08-05 12:43:03 -0500

    Rewriting codeblocks more succinctly

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -16,70 +16,62 @@ lazy_static! {
 /// Creates a new configuration if it does not already exist (first time setup), otherwise returns
 /// the existing configuration from the system.
 pub fn configure_fetters() -> Result<FettersSettings, FettersError> {
-    match ProjectDirs::from("", "", "fetters") {
-        Some(project_directory) => {
-            let config_path = project_directory.config_dir().join("fetters.toml");
+    let project_directory =
+        ProjectDirs::from("", "", "fetters").ok_or(FettersError::ApplicationError)?;
 
-            if !config_path.exists() {
-                match &config_path.parent() {
-                    Some(parent) => fs::create_dir_all(parent)?,
-                    None => {
-                        return Err(FettersError::PathError(
-                            "Failed to retrieve fetters application directory path!".to_string(),
-                        ))
-                    }
-                }
+    let config_path = project_directory.config_dir().join("fetters.toml");
 
-                fs::write(&config_path, *TOML_CONFIG)?;
+    if !config_path.exists() {
+        match &config_path.parent() {
+            Some(parent) => fs::create_dir_all(parent)?,
+            None => {
+                return Err(FettersError::PathError(
+                    "Failed to retrieve fetters application directory path!".to_string(),
+                ))
             }
-
-            Ok(FettersSettings::read_from(
-                config_path.to_string_lossy().to_string(),
-            )?)
         }
-        None => Err(FettersError::ApplicationError),
+
+        fs::write(&config_path, *TOML_CONFIG)?;
     }
+
+    let fetters_settings = FettersSettings::read_from(config_path.to_string_lossy().to_string())?;
+
+    Ok(fetters_settings)
 }
 
 /// Add a new job status to the TOML configuration file.
 pub fn add_new_job_status(new_status: &str, new_color: &str) -> Result<(), FettersError> {
-    match ProjectDirs::from("", "", "fetters") {
-        Some(project_directory) => {
-            let config_path = project_directory.config_dir().join("fetters.toml");
+    let project_directory =
+        ProjectDirs::from("", "", "fetters").ok_or(FettersError::ApplicationError)?;
+    let config_path = project_directory.config_dir().join("fetters.toml");
 
-            let mut config = FettersSettings::read_from(config_path.to_string_lossy().to_string())?;
+    let mut config = FettersSettings::read_from(config_path.to_string_lossy().to_string())?;
 
-            let fixed_status = new_status.replace(' ', "-").to_lowercase();
-            let fixed_color = new_color.to_lowercase();
+    let fixed_status = new_status.replace(' ', "-").to_lowercase();
+    let fixed_color = new_color.to_lowercase();
 
-            config
-                .presets
-                .status_mappings
-                .insert(fixed_status, fixed_color);
+    config
+        .presets
+        .status_mappings
+        .insert(fixed_status, fixed_color);
 
-            let toml_string = toml::to_string_pretty(&config)?;
-            fs::write(&config_path, toml_string)?;
+    let toml_string = toml::to_string_pretty(&config)?;
+    fs::write(&config_path, toml_string)?;
 
-            Ok(())
-        }
-        None => Err(FettersError::ApplicationError),
-    }
+    Ok(())
 }
 
 /// Add a new job title to the TOML configuration file.
 pub fn add_new_job_title(new_title: &str) -> Result<(), FettersError> {
-    match ProjectDirs::from("", "", "fetters") {
-        Some(project_directory) => {
-            let config_path = project_directory.config_dir().join("fetters.toml");
+    let project_directory =
+        ProjectDirs::from("", "", "fetters").ok_or(FettersError::ApplicationError)?;
+    let config_path = project_directory.config_dir().join("fetters.toml");
 
-            let mut config = FettersSettings::read_from(config_path.to_string_lossy().to_string())?;
-            config.presets.titles.push(new_title.to_string());
+    let mut config = FettersSettings::read_from(config_path.to_string_lossy().to_string())?;
+    config.presets.titles.push(new_title.to_string());
 
-            let toml_string = toml::to_string_pretty(&config)?;
-            fs::write(&config_path, toml_string)?;
+    let toml_string = toml::to_string_pretty(&config)?;
+    fs::write(&config_path, toml_string)?;
 
-            Ok(())
-        }
-        None => Err(FettersError::ApplicationError),
-    }
+    Ok(())
 }
