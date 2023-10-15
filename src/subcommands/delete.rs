cbeafad2de98f3cfee44299f8460bec22a6ed221
Joseph Lai <jlai24142@gmail.com>
2023-10-14 19:08:44 -0500

    Fleshing out pattern matching condition for the delete job functionality

--- src/subcommands/delete.rs
+++ src/subcommands/delete.rs
@@ -4,7 +4,8 @@ use std::collections::{BTreeMap, HashMap};
 
 use ansi_term::{Color, Style};
 use diesel::SqliteConnection;
-use inquire::Confirm;
+use inquire::{Confirm, Select};
+use lazy_static::lazy_static;
 use regex::Regex;
 
 use crate::{
@@ -13,6 +14,13 @@ use crate::{
     sqlite::{self},
     utils,
 };
+
+lazy_static! {
+    /// The regex expression used for extracting the job ID from the square brackets in the
+    /// `Select` menu's option list.
+    static ref JOB_ID_REGEX: Regex = Regex::new(r"\[(\d+)\]")
+        .expect("FAILED TO CREATE THE REGEX EXPRESSION TO MATCH JOB IDS FROM THE SELECTION MENU!");
+}
 
 /// Delete a job from the SQLite database.
 pub fn delete_job(
@@ -38,17 +46,17 @@ pub fn delete_job(
 
     let mut matched_jobs: BTreeMap<Option<i32>, SearchResult> = BTreeMap::new();
 
-    if let Some(id) = id {
+    if let Some(job_id) = id {
         if query.is_some() {
             println!(
                 "\n{}\n",
                 Color::Fixed(172)
                     .bold()
-                    .paint(format!("An ID and query were both provided! The ID will take precedence over the query."))
+                    .paint("An ID and query were both provided! The ID will take precedence over the query.".to_string())
             );
         }
 
-        if let Some(selected_job) = all_jobs.iter().find(|job| job.id == Some(id)) {
+        if let Some(selected_job) = all_jobs.iter().find(|job| job.id == Some(job_id)) {
             let stint_name = match selected_job.stint {
                 Some(id) => mapped_stints
                     .get(&Some(id))
@@ -71,7 +79,7 @@ pub fn delete_job(
             if skip_confirmation {
                 println!("{}\n", Color::Cyan.bold().paint("DELETING JOB."));
 
-                sqlite::queries::delete_job(connection, id)?;
+                sqlite::queries::delete_job(connection, job_id)?;
 
                 println!(
                     "{}\n",
@@ -79,37 +87,35 @@ pub fn delete_job(
                         .bold()
                         .paint("🚮 Successfully deleted the job!")
                 );
-            } else {
-                if let Some(confirm_deletion) = Confirm::new("Confirm deletion?")
-                    .with_default(true)
-                    .prompt_skippable()
-                    .ok()
-                    .flatten()
-                {
-                    if confirm_deletion {
-                        println!("\n{}\n", Color::Cyan.bold().paint("DELETING JOB."));
+            } else if let Some(confirm_deletion) = Confirm::new("Confirm deletion?")
+                .with_default(true)
+                .prompt_skippable()
+                .ok()
+                .flatten()
+            {
+                if confirm_deletion {
+                    println!("\n{}\n", Color::Cyan.bold().paint("DELETING JOB."));
 
-                        sqlite::queries::delete_job(connection, id)?;
+                    sqlite::queries::delete_job(connection, job_id)?;
 
-                        println!(
-                            "{}\n",
-                            Color::Green
-                                .bold()
-                                .paint("🚮 Successfully deleted the job!")
-                        );
-                    } else {
-                        println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
-                    }
+                    println!(
+                        "{}\n",
+                        Color::Green
+                            .bold()
+                            .paint("🚮 Successfully deleted the job!")
+                    );
                 } else {
                     println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
                 }
+            } else {
+                println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
             }
         } else {
             println!(
                 "\n{}\n",
                 Color::Red
                     .bold()
-                    .paint(format!("NO JOB WITH ID {id} FOUND!"))
+                    .paint(format!("NO JOB WITH ID {job_id} FOUND!"))
             );
         }
     } else if let Some(pattern) = query {
@@ -173,7 +179,70 @@ pub fn delete_job(
         if matched_jobs.is_empty() {
             println!("\n{}\n", Color::Red.bold().paint("No matches were found!"));
         } else {
+            let matched_options: Vec<String> = matched_jobs
+                .iter()
+                .map(|(job_id, search_result)| {
+                    if let Some(id) = job_id {
+                        format!("[{}] {}", id, search_result.job.company)
+                    } else {
+                        "???".to_string()
+                    }
+                })
+                .collect();
+
             utils::display::display_queried_jobs(matched_jobs, &status_mappings, &mut table);
+
+            if let Some(selection) = Select::new("Select a job to delete", matched_options)
+                .prompt_skippable()
+                .ok()
+                .flatten()
+            {
+                if let Some(captures) = JOB_ID_REGEX.captures(&selection) {
+                    let job_id_text = captures
+                        .get(1)
+                        .map(|text| text.as_str())
+                        .ok_or(FettersError::JobIDCaptureError)?;
+
+                    let job_id = job_id_text.parse::<i32>()?;
+
+                    if skip_confirmation {
+                        println!("{}\n", Color::Cyan.bold().paint("DELETING JOB."));
+
+                        sqlite::queries::delete_job(connection, job_id)?;
+
+                        println!(
+                            "{}\n",
+                            Color::Green
+                                .bold()
+                                .paint("🚮 Successfully deleted the job!")
+                        );
+                    } else if let Some(confirm_deletion) = Confirm::new("Confirm deletion?")
+                        .with_default(true)
+                        .prompt_skippable()
+                        .ok()
+                        .flatten()
+                    {
+                        if confirm_deletion {
+                            println!("\n{}\n", Color::Cyan.bold().paint("DELETING JOB."));
+
+                            sqlite::queries::delete_job(connection, job_id)?;
+
+                            println!(
+                                "{}\n",
+                                Color::Green
+                                    .bold()
+                                    .paint("🚮 Successfully deleted the job!")
+                            );
+                        } else {
+                            println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
+                        }
+                    } else {
+                        println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
+                    }
+                }
+            } else {
+                println!("\n{}\n", Color::Red.bold().paint("CANCELLING."));
+            }
         }
     }
 
