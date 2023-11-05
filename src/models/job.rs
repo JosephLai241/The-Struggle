83a0873f77ea6c01b2e897ad07ee29017cbc52b0
Joseph Lai <jlai24142@gmail.com>
2023-11-05 15:36:47 -0600

    Deriving AsChangeset for the Job struct. Adding a new UpdateJob struct for the update subcommand

--- src/models/job.rs
+++ src/models/job.rs
@@ -1,13 +1,14 @@
 //! Contains all models pertaining to job listings.
 
 use chrono::Local;
-use diesel::{sqlite::Sqlite, Insertable, Queryable, Selectable};
+use diesel::{query_builder::AsChangeset, sqlite::Sqlite, Insertable, Queryable, Selectable};
 use serde::{Deserialize, Serialize};
 
 use crate::schema::job_data;
 
 /// Contains all attributes associated with a job application.
 #[derive(
+    AsChangeset,
     Clone,
     Debug,
     Deserialize,
@@ -62,4 +63,21 @@ impl Job {
             title,
         }
     }
+}
+
+/// Contains fields that may be updatable via the `update` subcommand.
+#[derive(Debug)]
+pub struct UpdateJob {
+    /// The new company name.
+    pub company: Option<String>,
+    /// The new link associated with a job listing.
+    pub link: Option<String>,
+    /// New notes associated with a job listing.
+    pub notes: Option<String>,
+    /// The new job listing status.
+    pub status: Option<String>,
+    /// The new stint name.
+    pub stint: Option<String>,
+    /// The new job title.
+    pub title: Option<String>,
 }
