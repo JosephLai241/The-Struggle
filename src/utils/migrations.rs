12ce7e5f7b325e7fd3fa36f197baafa330b9a6f0
Joseph Lai <jlai24142@gmail.com>
2025-08-12 19:33:24 -0500

    Adding a function to run migrations on startup

---
+++ src/utils/migrations.rs
@@ -0,0 +1,26 @@
+//! Contains a utility function for running migrations on startup.
+
+use diesel::SqliteConnection;
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
+use owo_colors::OwoColorize;
+
+use crate::errors::FettersError;
+
+/// Migrations to include with `fetters`. These migrations will be run on startup.
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
+
+/// Run the SQLite migrations specified in the `migrations/` directory.
+pub fn run_migrations(connection: &mut SqliteConnection) -> Result<(), FettersError> {
+    if let Err(error) = connection.run_pending_migrations(MIGRATIONS) {
+        println!(
+            "{}",
+            format!("FAILED TO RUN SQLITE MIGRATIONS: {}", error.to_string())
+                .red()
+                .bold()
+        );
+
+        return Err(FettersError::MigrationFailure);
+    }
+
+    Ok(())
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,5 @@
 //! Contains all utils for `fetters`.
 
 pub mod display;
+pub mod migrations;
 pub mod titles;
