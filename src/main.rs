cb4d3ee29a7deffdabe501e4401dc6bc66e91ac2
Joseph Lai <jlai24142@gmail.com>
2025-08-12 19:33:41 -0500

    Putting shit together

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,21 @@ use clap::Parser;
 use lazy_static::lazy_static;
 use owo_colors::OwoColorize;
 
-use crate::cli::{Cli, Command};
+use crate::cli::{Cli, Command, ConfigOption, SprintOption};
 use crate::commands::add::add_job;
+use crate::commands::config::edit_config;
 use crate::commands::delete::delete_job;
 use crate::commands::list::list_jobs;
 use crate::commands::open::open_application;
+use crate::commands::sprint::{
+    create_new_sprint, set_sprint, show_all_sprints, show_current_sprint,
+};
 use crate::commands::update::update_job;
 use crate::config::configuration::Config;
 use crate::errors::FettersError;
 use crate::repositories::{sprint::SprintRepository, statuses::StatusRepository};
 use crate::sqlite::Database;
+use crate::utils::migrations::run_migrations;
 
 lazy_static! {
     /// ASCII art for `fetters`.
@@ -35,6 +40,8 @@ fn main() -> Result<(), FettersError> {
     let config = Config::load_or_create()?;
     let mut database = Database::new_connection(&config.db_path)?;
 
+    run_migrations(&mut database.connection)?;
+
     // Ensure the default statuses are stored in the `statuses` SQLite table.
     let mut status_repo = StatusRepository {
         connection: &mut database.connection,
@@ -44,7 +51,7 @@ fn main() -> Result<(), FettersError> {
     let mut sprint_repo = SprintRepository {
         connection: &mut database.connection,
     };
-    let current_sprint = sprint_repo.get_current_sprint(&config.sprint)?;
+    let current_sprint = sprint_repo.get_current_sprint(&config.current_sprint)?;
 
     let cli = Cli::parse();
 
@@ -54,7 +61,17 @@ fn main() -> Result<(), FettersError> {
                 println!("{}", error.red().bold());
             }
         }
-        Command::Config { show } => (),
+        Command::Banner => println!("{}", String::from_utf8_lossy(&ASCII_ART[..]).red().bold()),
+        Command::Config(config_option) => match config_option {
+            ConfigOption::Edit => {
+                if let Err(error) = edit_config() {
+                    println!("{}", error.red().bold());
+                }
+            }
+            ConfigOption::Show => {
+                println!("{config:#?}");
+            }
+        },
         Command::Delete(mut query_args) => {
             if let Err(error) =
                 delete_job(&mut database.connection, &mut query_args, &current_sprint)
@@ -74,12 +91,28 @@ fn main() -> Result<(), FettersError> {
                 println!("{}", error.red().bold());
             }
         }
-        Command::Sprint {
-            current,
-            new,
-            show_all,
-            set,
-        } => (),
+        Command::Sprint(sprint_option) => match sprint_option {
+            SprintOption::Current => {
+                show_current_sprint(current_sprint);
+            }
+            SprintOption::New { name } => {
+                if let Err(error) =
+                    create_new_sprint(&mut database.connection, &name, config, &current_sprint)
+                {
+                    println!("{}", error.red().bold());
+                }
+            }
+            SprintOption::ShowAll => {
+                if let Err(error) = show_all_sprints(&mut database.connection) {
+                    println!("{}", error.red().bold());
+                }
+            }
+            SprintOption::Set => {
+                if let Err(error) = set_sprint(&mut database.connection, config) {
+                    println!("{}", error.red().bold());
+                }
+            }
+        },
         Command::Update(mut query_args) => {
             if let Err(error) =
                 update_job(&mut database.connection, &mut query_args, &current_sprint)
